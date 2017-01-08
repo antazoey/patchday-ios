742b976960e4328e6443036042a4577cf59a38e9
Juliya Smith <Yingthi@khaleespo.local>
2017-01-08 12:19:44 -0600

    Initial Commit

---
+++ PatchDay/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PatchDay
+//
+//  Created by Juliya Smith on 1/8/17.
+//  Copyright © 2017 Juliya Smith. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
