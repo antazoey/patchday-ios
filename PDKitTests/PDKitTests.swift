71b32d6d81211ecf7ce030d1341646fdb3a623b4
Juliya Smith <parallel@crania.local>
2018-12-21 03:13:16 -0600

    Reset version for PDKit tests

--- PDKitTests/PDKitTests.swift
+++ PDKitTests/PDKitTests.swift
@@ -2,35 +2,32 @@
 //  PDKitTests.swift
 //  PDKitTests
 //
-//  Created by Juliya Smith on 6/20/18.
+//  Created by Juliya Smith on 12/21/18.
 //  Copyright © 2018 Juliya Smith. All rights reserved.
 //
 
 import XCTest
-@testable import PDKit
 
 class PDKitTests: XCTestCase {
-    
+
     override func setUp() {
-        super.setUp()
         // Put setup code here. This method is called before the invocation of each test method in the class.
     }
-    
+
     override func tearDown() {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
-        super.tearDown()
     }
-    
+
     func testExample() {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
     }
-    
+
     func testPerformanceExample() {
         // This is an example of a performance test case.
         self.measure {
             // Put the code you want to measure the time of here.
         }
     }
-    
+
 }
