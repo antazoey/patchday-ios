d371745fdeaa1dc26d18d1a17dd7e593023bbac8
Juliya Smith <parallel@crania.local>
2018-12-23 11:29:17 -0600

    Else lint

--- PDKit/Helpers/PDPillHelper.swift
+++ PDKit/Helpers/PDPillHelper.swift
@@ -18,27 +18,23 @@ public class PDPillHelper: NSObject {
     /// Return the next time the pill is due.
     public static func nextDueDate(timesTakenToday: Int, timesaday: Int, times: [NSDate?]) -> Date? {
         if times.count > 0, let time1 = times[0] as Time? {
-            // One-a-day
+            // One-a-day, either due today or tomorrow
             if timesaday == 1 {
                 if timesTakenToday == 0 {
                     return PDDateHelper.getDate(on: Date(), at: time1)
-                }
-                // Take tomorrow
-                else if let todayTime = PDDateHelper.getDate(on: Date(), at: time1) {
+                } else if let todayTime = PDDateHelper.getDate(on: Date(), at: time1) {
                     return PDDateHelper.getDate(at: todayTime, daysToAdd: 1)
                 }
             }
-            // Two-a-day
+            // Two-a-day, figure out if it's time 1 or 2, or if tomorrow
             else if times.count >= 1, let time2 = times[1] as Time? {
                 if timesTakenToday == 0 {
                     let minTime = min(time1, time2)
                     return PDDateHelper.getDate(on: Date(), at: minTime)
-                }
-                else if timesTakenToday == 1 {
+                } else if timesTakenToday == 1 {
                     let maxTime = max(time1, time2)
                     return PDDateHelper.getDate(on: Date(), at: maxTime)
-                }
-                else {
+                } else {
                     let minTime = min(time1, time2)
                     if let todayTime = PDDateHelper.getDate(on: Date(), at: minTime) {
                         return PDDateHelper.getDate(at: todayTime, daysToAdd: 1)
