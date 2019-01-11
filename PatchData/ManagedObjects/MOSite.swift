4eb8e2a4876449719b58a7b80859b530307030b6
Juliya <juliya@juliyasmith.com>
2019-01-10 20:37:53 -0600

    Improved site comparability

--- PatchData/ManagedObjects/MOSite.swift
+++ PatchData/ManagedObjects/MOSite.swift
@@ -15,38 +15,91 @@ public typealias SiteName = String
 @objc(MOSite)
 public class MOSite: NSManagedObject, Comparable {
     
+/* Note: In MOSites, we want negative orders and nil orders
+        to be at the end of a sort, then negative orders are > positive orders
+     and I know that sounds weird, but we are kind of backwards here at PatchDay Inc.
+ */
+    
     public static func < (lhs: MOSite, rhs: MOSite) -> Bool {
         switch(lhs.order, rhs.order) {
+        // both not set
         case (nil, nil) : return false
+        case (let neg1, let neg2) where neg1 < 0 && neg2 < 0 : return false
+        case (nil, let neg) where neg < 0 : return false
+        case (let neg, nil) where neg < 0 : return false
+            
+        // left field not set
         case (nil, _) : return false
+        case (let neg, _) where neg < 0 : return false
+
+        // right field not set
         case (_, nil) : return true
+        case (_, let neg) where neg < 0 : return true
+
+        // both fields sets
         default : return lhs.order < rhs.order
         }
     }
     
     public static func > (lhs: MOSite, rhs: MOSite) -> Bool {
         switch(lhs.order, rhs.order) {
+        // both not set
         case (nil, nil) : return false
+        case (let neg1, let neg2) where neg1 < 0 && neg2 < 0 : return false
+        case (nil, let neg) where neg < 0 : return false
+        case (let neg, nil) where neg < 0 : return false
+            
+        // left field not set
         case (nil, _) : return true
+        case (let neg, _) where neg < 0 : return true
+            
+        // right field not set
         case (_, nil) : return false
+        case (_, let neg) where neg < 0 : return false
+            
+        // both fields sets
         default : return lhs.order > rhs.order
         }
     }
     
     public static func == (lhs: MOSite, rhs: MOSite) -> Bool {
         switch(lhs.order, rhs.order) {
+        // both not set
         case (nil, nil) : return true
+        case (let neg1, let neg2) where neg1 < 0 && neg2 < 0 : return true
+        case (nil, let neg) where neg < 0 : return true
+        case (let neg, nil) where neg < 0 : return true
+            
+        // left field not set
         case (nil, _) : return false
+        case (let neg, _) where neg < 0 : return false
+            
+        // right field not set
         case (_, nil) : return false
+        case (_, let neg) where neg < 0 : return false
+            
+        // both fields sets
         default : return lhs.order == rhs.order
         }
     }
     
     public static func != (lhs: MOSite, rhs: MOSite) -> Bool {
         switch(lhs.order, rhs.order) {
+        // both not set
         case (nil, nil) : return false
+        case (let neg1, let neg2) where neg1 < 0 && neg2 < 0 : return false
+        case (nil, let neg) where neg < 0 : return false
+        case (let neg, nil) where neg < 0 : return false
+            
+        // left field not set
         case (nil, _) : return true
+        case (let neg, _) where neg < 0 : return true
+            
+        // right field not set
         case (_, nil) : return true
+        case (_, let neg) where neg < 0 : return true
+            
+        // both fields sets
         default : return lhs.order != rhs.order
         }
     }
--- PatchDataTests/ManagedObjects/MOSiteTests.swift
+++ PatchDataTests/ManagedObjects/MOSiteTests.swift
@@ -34,19 +34,107 @@ class MOSiteTests: XCTestCase {
     }
     
     func testLT() {
-        let s1 = siteSchedule.insert()
-        let s2 = siteSchedule.insert()
-        s1?.setOrder(to: 0)
-        s2?.setOrder(to: 1)
-        XCTAssert(s1! < s2!)
+        let site_lower = siteSchedule.insert()!
+        let site_higher = siteSchedule.insert()!
+        let site_negOrder = siteSchedule.insert()!
+        let site_nilOrder = siteSchedule.insert()!
+        site_lower.setOrder(to: 0)
+        site_higher.setOrder(to: 1)
+        site_negOrder.setOrder(to: -23)
+        site_nilOrder.reset()
+        // Sites with lower orders are less than
+        XCTAssert(site_lower < site_higher)
+        // all positive orders are less nil orders
+        XCTAssert(site_lower < site_nilOrder)
+        XCTAssert(site_higher < site_nilOrder)
+        // All positives orders are less than Sites with negative orders
+        // ( I know that sounds backwords, see MOSites desc.)
+        XCTAssert(site_lower < site_negOrder)
+        XCTAssert(site_higher < site_negOrder)
+        // Negative orders are not greater than nil orders and visa versa
+        XCTAssertFalse(site_negOrder < site_nilOrder)
+        XCTAssertFalse(site_nilOrder < site_negOrder)
     }
     
     func testGT() {
-        let s1 = siteSchedule.insert()
-        let s2 = siteSchedule.insert()
-        s1?.setOrder(to: 0)
-        s2?.setOrder(to: 1)
-        XCTAssert(s2! > s1!)
+        let site_lower = siteSchedule.insert()!
+        let site_higher = siteSchedule.insert()!
+        let site_negOrder = siteSchedule.insert()!
+        let site_nilOrder = siteSchedule.insert()!
+        site_lower.setOrder(to: 0)
+        site_higher.setOrder(to: 1)
+        site_negOrder.setOrder(to: -23)
+        site_nilOrder.reset()
+        // Sites with higher orders are greater than
+        XCTAssert(site_higher > site_lower)
+        // nil orders are greater than positive orders
+        XCTAssert(site_nilOrder > site_lower)
+        XCTAssert(site_nilOrder > site_higher)
+        // Sites with negative orders are greater than sites with positive orders.
+        // ( I know that sounds backwords, see MOSites desc.)
+        XCTAssert(site_negOrder > site_lower)
+        XCTAssert(site_negOrder > site_higher)
+        // Negative orders are not greater than nil orders and visa versa
+        XCTAssertFalse(site_nilOrder > site_negOrder)
+        XCTAssertFalse(site_negOrder > site_nilOrder)
+    }
+    
+    func testEQ() {
+        let site_lower = siteSchedule.insert()!
+        let site_sameAsLower = siteSchedule.insert()!
+        let site_higher = siteSchedule.insert()!
+        let site_negOrder = siteSchedule.insert()!
+        let site_diffNegOrder = siteSchedule.insert()!
+        let site_nilOrder = siteSchedule.insert()!
+        site_lower.setOrder(to: 0)
+        site_sameAsLower.setOrder(to: 0)
+        site_higher.setOrder(to: 1)
+        site_negOrder.setOrder(to: -23)
+        site_diffNegOrder.setOrder(to: -19)
+        site_nilOrder.reset()
+        // Sites with the same order are equal
+        XCTAssert(site_lower == site_sameAsLower)
+        XCTAssert(site_sameAsLower == site_lower)
+        // Sites with different orders are not equal
+        XCTAssertFalse(site_lower == site_higher)
+        XCTAssertFalse(site_higher == site_lower)
+        // Sites with positive orders are not equal to sites with nil orders
+        XCTAssertFalse(site_lower == site_nilOrder)
+        XCTAssertFalse(site_nilOrder == site_higher)
+        // Sites with negative orders are equal regardless of value
+        XCTAssert(site_negOrder == site_diffNegOrder)
+        // Sites with negative orders are equal to sites with nil orders
+        XCTAssert(site_negOrder == site_nilOrder)
+        XCTAssert(site_nilOrder == site_negOrder)
+    }
+    
+    func testNQ() {
+        let site_lower = siteSchedule.insert()!
+        let site_sameAsLower = siteSchedule.insert()!
+        let site_higher = siteSchedule.insert()!
+        let site_negOrder = siteSchedule.insert()!
+        let site_diffNegOrder = siteSchedule.insert()!
+        let site_nilOrder = siteSchedule.insert()!
+        site_lower.setOrder(to: 0)
+        site_sameAsLower.setOrder(to: 0)
+        site_higher.setOrder(to: 1)
+        site_negOrder.setOrder(to: -23)
+        site_diffNegOrder.setOrder(to: -19)
+        site_nilOrder.reset()
+        // Sites with the same order are equal
+        XCTAssertFalse(site_lower != site_sameAsLower)
+        XCTAssertFalse(site_sameAsLower != site_lower)
+        // Sites with different orders are not equal
+        XCTAssert(site_lower != site_higher)
+        XCTAssert(site_higher != site_lower)
+        // Sites with positive orders are not equal to sites with nil orders
+        XCTAssert(site_lower != site_nilOrder)
+        XCTAssert(site_nilOrder != site_higher)
+        // Sites with negative orders are equal regardless of value
+        XCTAssertFalse(site_negOrder != site_diffNegOrder)
+        // Sites with negative orders are equal to sites with nil orders
+        XCTAssertFalse(site_negOrder != site_nilOrder)
+        XCTAssertFalse(site_nilOrder != site_negOrder)
     }
     
     func testIsOccupied() {
