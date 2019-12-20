c1a13319d184a321c984c635720a519f151a5501
unparalleled-js <yingthi@live.com>
2019-12-19 21:06:17 -0600

    Init mock proj

---
+++ PDMockTests/PDMockTests.swift
@@ -0,0 +1,34 @@
+//
+//  PDMockTests.swift
+//  PDMockTests
+//
+//  Created by Juliya Smith on 12/19/19.
+//  Copyright © 2019 Juliya Smith. All rights reserved.
+//
+
+import XCTest
+@testable import PDMock
+
+class PDMockTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
