e091a5a16e76bae50c97095f46af031d1c93f244
unparalleled-js <yingthi@live.com>
2020-02-15 20:04:27 -0600

    exp interval tests

--- PDKitTests/UserDefaults/Complex/ExpirationIntervalUDTests.swift
+++ PDKitTests/UserDefaults/Complex/ExpirationIntervalUDTests.swift
@@ -5,5 +5,24 @@
 
 import Foundation
 
-class ExpirationIntervalUD {
+import XCTest
+@testable
+import PDKit
+
+class ExpirationIntervalUDTests: XCTestCase {
+
+    func testHours_whenIsEveryTwoWeeks_returnsExpectedHours() {
+        let expiration = ExpirationIntervalUD(.EveryTwoWeeks)
+        XCTAssertEqual(336, expiration.hours)
+    }
+
+    func testHours_whenIsOnceAWeek_returnsExpectedHours() {
+        let expiration = ExpirationIntervalUD(.OnceAWeek)
+        XCTAssertEqual(168, expiration.hours)
+    }
+
+    func testHours_whenIsTwiceAWeek_returnsExpectedHours() {
+        let expiration = ExpirationIntervalUD(.TwiceAWeek)
+        XCTAssertEqual(84, expiration.hours)
+    }
 }
