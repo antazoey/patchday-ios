8ca31483878c83dbc3623d899916e5ebd986db4c
unparalleled-js <yingthi@live.com>
2021-03-15 09:41:06 -0500

    break out tests

--- Tests/PDKitTests/Settings/SettingsOptionsTests.swift
+++ Tests/PDKitTests/Settings/SettingsOptionsTests.swift
@@ -11,12 +11,20 @@ import XCTest
 import PDKit
 
 class SettingsOptionsTests: XCTestCase {
-    func testGetExpirationInterval() {
+    func testGetExpirationInterval_whenGivenOnceDaily_returnsExpectedInterval() {
         XCTAssertEqual(.OnceDaily, SettingsOptions.getExpirationInterval(for: "Once daily"))
+    }
+
+    func testGetExpirationInterval_whenGivenTwiceWeekly_returnsExpectedInterval() {
         XCTAssertEqual(.TwiceWeekly, SettingsOptions.getExpirationInterval(for: "Twice weekly"))
+    }
+
+    func testGetExpirationInterval_whenGivenOnceWeekly_returnsExpectedInterval() {
         XCTAssertEqual(.OnceWeekly, SettingsOptions.getExpirationInterval(for: "Once weekly"))
-        XCTAssertEqual(.EveryTwoWeeks, SettingsOptions.getExpirationInterval(
-            for: "Once every two weeks")
-        )
+    }
+
+    func testGetExpirationInterval_whenGivenEveryTwoWeeks_returnsExpectedInterval() {
+        let actual = SettingsOptions.getExpirationInterval(for: "Once every two weeks")
+        XCTAssertEqual(.EveryTwoWeeks, actual)
     }
 }
