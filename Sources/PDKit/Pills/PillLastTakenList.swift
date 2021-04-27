e00a2e7ac94ad0d6315df8cfffe85a1891177861
unparalleled-js <yingthi@live.com>
2021-04-27 17:44:00 -0500

    Test pill last takens list

--- Sources/PDKit/Pills/PillLastTakenList.swift
+++ Sources/PDKit/Pills/PillLastTakenList.swift
@@ -9,29 +9,49 @@
 import Foundation
 
 public class PillLastTakenList {
-    private let dateString: String?
+    private var _dateString: String?
+    private var _dates: [Date]
 
-    public init(dateString: String?) {
-        self.dateString = dateString
+    public var dateString: String {
+        _dateString ?? ""
     }
 
     public var dates: [Date] {
-        guard let lastTakensString = dateString else { return [] }
-        return DateFactory.createDatesFromCommaSeparatedString(lastTakensString)
+        _dates
     }
 
+    public init(dateString: String?) {
+        self._dateString = dateString
+        if let dateString = dateString {
+            self._dates = DateFactory.createDatesFromCommaSeparatedString(dateString)
+        } else {
+            self._dates = []
+        }
+    }
+
+    public var count: Int {
+        _dates.count
+    }
+
+    /// Extract the last date off the list and get a tuple of that date and the new date string.
+    @discardableResult
     public func splitLast() -> (Date?, String?) {
-        var current = dates
-        let last = current.popLast()
-        let newString = PDDateFormatter.convertDatesToCommaSeparatedString(current)
+        let last = _dates.popLast()
+        let newString = PDDateFormatter.convertDatesToCommaSeparatedString(_dates)
+        self._dateString = newString
         return (last, newString)
     }
 
+    /// Append a date to the last takens list and comma-separated date string.
+    @discardableResult
     public func combineWith(lastTaken: Date?) -> String? {
         guard dates.count < MaxPillTimesaday else { return nil }
         guard let lastTaken = lastTaken else { return nil }
-        let formattedDate = PDDateFormatter.formatDate(lastTaken)
-        guard let original = dateString else { return formattedDate }
-        return "\(original),\(formattedDate)"
+        let formattedDate = ISO8601DateFormatter().string(from: lastTaken)
+        guard let original = _dateString else { return formattedDate }
+        let newDateString = "\(original),\(formattedDate)"
+        self._dateString = newDateString
+        self._dates = DateFactory.createDatesFromCommaSeparatedString(newDateString)
+        return newDateString
     }
 }
