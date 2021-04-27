5976a29004df333f3450f3a54a0716c73c095f0e
unparalleled-js <yingthi@live.com>
2021-04-26 19:49:13 -0500

    Fix bug in quotient class

--- Sources/PDKit/Pills/PillTimesQuotient.swift
+++ Sources/PDKit/Pills/PillTimesQuotient.swift
@@ -8,9 +8,11 @@
 
 import Foundation
 
+/// A class abstracting the quotient between a pill's `timesTakenToday` and a pill's `timesday`.
 public class PillTimesQuotient {
     private var _timesTakenToday: Int
     private var _timesaday: Int
+    private let sep = NSLocalizedString(" of ", comment: "Replace with ' / ' for non-En.")
 
     private var timesTakenToday: Int {
         if _timesTakenToday < 0 {
@@ -23,7 +25,7 @@ public class PillTimesQuotient {
     }
 
     private var timesaday: Int {
-        if _timesaday < 0 {
+        if (_timesaday < 0) {
             _timesaday = 1
         }
         return _timesaday
@@ -34,12 +36,20 @@ public class PillTimesQuotient {
         self._timesaday = timesaday
     }
 
+    /// The quotient string between `timesTakenToday` and `timesaday`,
+    /// such as `1 of 3`.
     public func toString() -> String {
-        let minDefaultString = "0 / \(timesaday))"
-        let maxDefaultString = "\(timesaday)) / \(timesaday))"
+        let minDefaultString = makeQuotientString(0, timesaday)
+        let maxDefaultString = makeQuotientString(timesaday, timesaday)
+
         guard timesTakenToday >= 0 else { return minDefaultString }
         guard timesaday >= 0 else { return minDefaultString }
         guard timesaday >= timesTakenToday else { return maxDefaultString }
-        return "\(timesTakenToday) / \(timesaday)"
+
+        return makeQuotientString(timesTakenToday, timesaday)
+    }
+
+    private func makeQuotientString(_ num: Int, _ denom: Int) -> String {
+        "\(num)\(sep)\(denom)"
     }
 }
--- Tests/PDKitTests/Pills/PillTimesQuotientTests.swift
+++ Tests/PDKitTests/Pills/PillTimesQuotientTests.swift
@@ -15,49 +15,49 @@ import PDKit
 class PillTimesQuotientTests: XCTestCase {
     func testToString_zeroOne_returnsExpectedString() {
         let quotient = PillTimesQuotient(timesTakenToday: 0, timesaday: 1)
-        let expected = "0 / 1"
+        let expected = "0 of 1"
         let actual = quotient.toString()
         XCTAssertEqual(expected, actual)
     }
 
     func testToString_oneOne_returnsExpectedString() {
         let quotient = PillTimesQuotient(timesTakenToday: 1, timesaday: 1)
-        let expected = "1 / 1"
+        let expected = "1 of 1"
         let actual = quotient.toString()
         XCTAssertEqual(expected, actual)
     }
 
     func testToString_threeFour_returnsExpectedString() {
         let quotient = PillTimesQuotient(timesTakenToday: 3, timesaday: 4)
-        let expected = "3 / 4"
+        let expected = "3 of 4"
         let actual = quotient.toString()
         XCTAssertEqual(expected, actual)
     }
 
     func testToString_fourFour_returnsExpectedString() {
         let quotient = PillTimesQuotient(timesTakenToday: 4, timesaday: 4)
-        let expected = "4 / 4"
+        let expected = "4 of 4"
         let actual = quotient.toString()
         XCTAssertEqual(expected, actual)
     }
 
     func testToString_twoOne_returnsExpectedDefaultString() {
         let quotient = PillTimesQuotient(timesTakenToday: 2, timesaday: 1)
-        let expected = "1 / 1"
+        let expected = "1 of 1"
         let actual = quotient.toString()
         XCTAssertEqual(expected, actual)
     }
 
     func testToString_negativeTimesTaken_returnsExpectedDefaultString() {
         let quotient = PillTimesQuotient(timesTakenToday: -2, timesaday: 3)
-        let expected = "0 / 3"
+        let expected = "0 of 3"
         let actual = quotient.toString()
         XCTAssertEqual(expected, actual)
     }
 
     func testToString_negativeTimesaday_returnsExpectedDefaultString() {
         let quotient = PillTimesQuotient(timesTakenToday: 2, timesaday: -3)
-        let expected = "1 / 1"
+        let expected = "1 of 1"
         let actual = quotient.toString()
         XCTAssertEqual(expected, actual)
     }
