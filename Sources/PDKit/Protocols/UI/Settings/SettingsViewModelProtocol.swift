8dfecddaf158908d7ae8eb94a664175a5deedde7
unparalleled-js <yingthi@live.com>
2021-05-02 15:33:03 -0500

    doc

--- Sources/PDKit/Protocols/UI/Settings/SettingsViewModelProtocol.swift
+++ Sources/PDKit/Protocols/UI/Settings/SettingsViewModelProtocol.swift
@@ -16,7 +16,7 @@ public protocol SettingsViewModelProtocol {
     /// The index to open the quantity picker at.
     var quantityStartIndex: Index { get }
 
-    /// The index to open the expiration interval at.
+    /// The index to open the expiration interval picker at.
     var expirationIntervalStartIndex: Index { get }
 
     /// Whether the expiration interval involves the use of the xDays property.
@@ -34,6 +34,6 @@ public protocol SettingsViewModelProtocol {
     /// Set the notifications setting.
     func setNotifications(_ newValue: Bool)
 
-    /// Set the notifications before minutes setting.
+    /// Set the notifications-before-minutes setting.
     func setNotificationsMinutes(_ newValue: Int)
 }
