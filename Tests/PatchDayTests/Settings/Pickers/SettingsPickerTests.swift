51ef09097861a4b6a88802ff8321fb98d58fdeaa
unparalleled-js <yingthi@live.com>
2021-05-19 16:10:00 -0500

    MORE TESTS

--- Tests/PatchDayTests/Settings/Pickers/SettingsPickerTests.swift
+++ Tests/PatchDayTests/Settings/Pickers/SettingsPickerTests.swift
@@ -33,12 +33,59 @@ class SettingsPickerTests: XCTestCase {
         XCTAssertEqual("2", picker[1].string)
     }
 
+    func testGetStartRow_defaultsToReturningZero() {
+        let picker = SettingsPicker()
+        let expected = 0
+        let actual = picker.getStartRow()
+        XCTAssertEqual(expected, actual)
+    }
+
+    func testGetStartRow_isSettable() {
+        let picker = SettingsPicker()
+        picker.getStartRow = { 1 }
+        let expected = 1
+        let actual = picker.getStartRow()
+        XCTAssertEqual(expected, actual)
+    }
+
+    func testOptions_returnsExpectedOptions() {
+        let expected = SettingsOptions.deliveryMethods
+        let picker = SettingsPicker()
+        picker.setting = .DeliveryMethod
+        let actual = picker.options
+        XCTAssertEqual(expected, actual)
+    }
+
+    func testCount_returnsExpectedOptions() {
+        let expected = SettingsOptions.deliveryMethods.count
+        let picker = SettingsPicker()
+        picker.setting = .DeliveryMethod
+        let actual = picker.count
+        XCTAssertEqual(expected, actual)
+    }
+
+    func testView_returnsSameInstanceAsSelf() {
+        let picker = SettingsPicker()
+        XCTAssertEqual(picker, picker.view)
+    }
+
     func testOpen_selectsActivator() {
         let activator = UIButton()
         activator.isSelected = false
-        let pickerView = SettingsPicker()
-        pickerView.activator = activator
-        pickerView.open()
+        let picker = SettingsPicker()
+        picker.activator = activator
+        picker.open()
         XCTAssertTrue(activator.isSelected)
+    }
+
+    func testOpen_callsGetStartRow() {
+        let picker = SettingsPicker()
+        var didCall = false
+        picker.getStartRow = {
+            didCall = true
+            return 0
+        }
+        picker.open()
+        XCTAssertTrue(didCall)
     }
 }
