e3a82797c5b301c3ad6b6e5c355dadb946d96eee
unparalleled-js <yingthi@live.com>
2021-08-07 19:46:05 -0500

    work on new integ test sys

--- Tests/PatchDayTests/Integration/IntegrationTests.swift
+++ Tests/PatchDayTests/Integration/IntegrationTests.swift
@@ -12,108 +12,156 @@ import PatchData
 @testable
 import PatchDay
 
-// swiftlint:disable function_body_length
 class WhenChangingHormoneBadge_UpdatesCorrectly: PDIntegrationTestCase {
     func test() {
+        sdk.settings.setDeliveryMethod(to: .Patches)
         let badge = PDBadge(sdk: sdk)
-        sdk.settings.setDeliveryMethod(to: .Patches)  // Should trigger reset to 3 patches
+        let ids = getIDs()
+        setDate(idIndex: 0, ids: ids)
+        setDate(idIndex: 1, ids: ids)
+        setDate(idIndex: 2, ids: ids)
+        badge.reflect()
+        assertBadgeValue(expected: 3, badge: badge)
+        sdk.hormones.setDate(at: 0, with: Date())
+        badge.reflect()
+        assertBadgeValue(expected: 2, badge: badge)
+    }
+
+    private func getIDs() -> [UUID] {
         let ids = self.sdk.hormones.all.map({ $0.id })
         if ids.count < 3 {
             XCTFail("Hormone count does not match delivery method")
-            return
+            return []
         }
-        sdk.hormones.setDate(by: ids[0], with: TestDateFactory.createTestDate(daysFrom: -20))
-        sdk.hormones.setDate(by: ids[1], with: TestDateFactory.createTestDate(daysFrom: -20))
-        sdk.hormones.setDate(by: ids[2], with: TestDateFactory.createTestDate(daysFrom: -20))
-        badge.reflect()
+        return ids
+    }
 
-        XCTAssertEqual(3, sdk.hormones.totalExpired)
-        XCTAssertEqual(3, badge.value)
+    private func setDate(idIndex: Index, ids: [UUID]) {
+        let testDate = TestDateFactory.createTestDate(daysFrom: -20)
+        sdk.hormones.setDate(by: ids[idIndex], with: testDate)
+    }
 
-        sdk.hormones.setDate(at: 0, with: Date())
-        badge.reflect()
-
-        XCTAssertEqual(2, sdk.hormones.totalExpired)
-        XCTAssertEqual(2, badge.value)
+    private func assertBadgeValue(expected: Int, badge: PDBadge) {
+        XCTAssertEqual(expected, sdk.hormones.totalExpired)
+        XCTAssertEqual(expected, badge.value)
     }
 }
 
 class WhenContinuingOnChangeDeliveryMethodAlert_addsOrRemoveHormonesToGetToDefaultQuantity: PDIntegrationTestCase {
     func test() {
-        let tabs = TabReflector(
+        let tabs = createTabs()
+        let handlers = createHandlers()
+        let patchesToGelAlert = createAlert(
+            tabs: tabs,
+            startDeliveryMethod: .Patches,
+            newDeliveryMethod: .Gel,
+            originalQuantity: sdk.settings.quantity.rawValue,
+            handlers: handlers
+        )
+        patchesToGelAlert.continueHandler()
+        assertQuantity(1)
+        let injectionsToPatchesAlert = createAlert(
+            tabs: tabs,
+            startDeliveryMethod: .Injections,
+            newDeliveryMethod: .Patches,
+            originalQuantity: 4,
+            handlers: handlers
+        )
+        injectionsToPatchesAlert.continueHandler()
+        assertQuantity(3)
+    }
+
+    func createTabs() -> TabReflector {
+        TabReflector(
             tabBarController: UITabBarController(),
             viewControllers: [UIViewController()],
             sdk: sdk
         )
-        let handlers = DeliveryMethodMutationAlertActionHandler { (_, _) in () }
-        let patchesToGelAlert = DeliveryMethodMutationAlert(
-            sdk: sdk,
-            tabs: tabs,
-            originalDeliveryMethod: .Patches,
-            originalQuantity: sdk.settings.quantity.rawValue,
-            newDeliveryMethod: .Gel,
-            handlers: handlers
-        )
-        patchesToGelAlert.continueHandler()
-        XCTAssertEqual(1, sdk.hormones.count)
-        XCTAssertEqual(1, sdk.settings.quantity.rawValue)
+    }
 
-        let injectionsToPatchesAlert = DeliveryMethodMutationAlert(
+    func createHandlers() -> DeliveryMethodMutationAlertActionHandler {
+        DeliveryMethodMutationAlertActionHandler { (_, _) in () }
+    }
+
+    func createAlert(
+        tabs: TabReflector,
+        startDeliveryMethod: DeliveryMethod,
+        newDeliveryMethod: DeliveryMethod,
+        originalQuantity: Int,
+        handlers: DeliveryMethodMutationAlertActionHandler
+    ) -> DeliveryMethodMutationAlert {
+        DeliveryMethodMutationAlert(
             sdk: sdk,
             tabs: tabs,
-            originalDeliveryMethod: .Injections,
-            originalQuantity: 4,
-            newDeliveryMethod: .Patches,
+            originalDeliveryMethod: startDeliveryMethod,
+            originalQuantity: originalQuantity,
+            newDeliveryMethod: newDeliveryMethod,
             handlers: handlers
         )
-        injectionsToPatchesAlert.continueHandler()
-        XCTAssertEqual(3, sdk.hormones.count)
-        XCTAssertEqual(3, sdk.settings.quantity.rawValue)
+    }
+
+    func assertQuantity(_ expected: Int) {
+        XCTAssertEqual(expected, sdk.hormones.count)
+        XCTAssertEqual(expected, sdk.settings.quantity.rawValue)
     }
 }
 
 class WhenTakingHormoneFromActionAlert_setsNotificationWithUpdatedDate: PDIntegrationTestCase {
+
+    let now = PDNow()
+    let style = UIUserInterfaceStyle.dark
+    let notifications = MockNotifications()
+    let alerts = MockAlertFactory()
+    let testDate = DateFactory.createDate(byAddingHours: -2, to: Date())!
+
     func test() {
         guard let hormone = sdk.hormones[0] else {
             XCTFail("This test required a hormone.")
             return
         }
+        setUpTest(hormone: hormone)
+        let viewModel = createViewModel()
+        let hormoneAfterTest = tapRowAndGetHormone(viewModel: viewModel, hormone: hormone)
+        userSelectChangeAction()
+        let actual = hormoneAfterTest.date
+        PDAssertNotEquiv(testDate, actual)
+    }
 
-        let notifications = MockNotifications()
-        let alerts = MockAlertFactory()
-        let testDate = DateFactory.createDate(byAddingHours: -2, to: Date())!
-        let now = PDNow()
-        let style = UIUserInterfaceStyle.dark
-        let table = HormonesTable(UITableView(), sdk, style)
-        let imageHistory = SiteImageHistory()
-        let dependencies = MockDependencies()
+    private func setUpTest(hormone: Hormonal) {
         sdk.settings.setUseStaticExpirationTime(to: false)
         dependencies.sdk = sdk
         dependencies.notifications = notifications
         dependencies.alerts = alerts
 
-        // Start off a hormone with a date.
+        // Start a hormone with a date
         sdk.hormones.setDate(by: hormone.id, with: testDate)
+    }
 
-        let hormonesViewModel = HormonesViewModel(
-            siteImageHistory: imageHistory,
+    private func createViewModel() -> HormonesViewModel {
+        HormonesViewModel(
+            siteImageHistory: SiteImageHistory(),
             style: style,
-            table: table,
+            table: createTable(),
             dependencies: dependencies,
             now: now
         )
+    }
 
-        hormonesViewModel.handleRowTapped(at: 0, UIViewController()) {}
+    func createTable() -> HormonesTable {
+        HormonesTable(UITableView(), sdk, style)
+    }
+
+    private func tapRowAndGetHormone(viewModel: HormonesViewModel, hormone: Hormonal) -> Hormonal {
+        viewModel.handleRowTapped(at: 0, UIViewController()) {}
         guard let hormoneAfterTest = sdk.hormones[hormone.id] else {
             XCTFail("Hormone somehow disappeared during test.")
-            return
+            return MockHormone()
         }
+        return hormoneAfterTest
+    }
 
+    private func userSelectChangeAction() {
         let changeAction = alerts.createHormoneActionsCallArgs[0].2
-        changeAction()  // Simulates user selecting "Change" from the alert
-
-        let actual = hormoneAfterTest.date
-        PDAssertNotEquiv(testDate, actual)
+        changeAction()
     }
 }
-// swiftlint:enable function_body_length
